cf31fbb0c112cdaccdecf5ea1c31856f466a84a0
bin <79569200+Bin-lin-rgb@users.noreply.github.com>
2022-06-22 23:14:36 +0800

    Update constrant.go

--- pkg/constrant/constrant.go
+++ pkg/constrant/constrant.go
@@ -4,6 +4,6 @@ import "time"
 
 const (
 	TokenExpireDuration = time.Hour * 24
-	MySQLDefaultDSN     = "root:123456@tcp(120.25.172.211)/douyin?charset=utf8&parseTime=True&loc=Local"
+	MySQLDefaultDSN     = "root:xxxxxx@tcp(xxx.xx.xxx.xxx)/douyin?charset=utf8&parseTime=True&loc=Local"
 	Root                = "http://10.0.2.2:8080/static/"
 )
